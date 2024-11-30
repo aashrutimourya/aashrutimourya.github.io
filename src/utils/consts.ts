8ae149fec79bbc46eeec218d5aad9cd169378cb2
raven7 <lkumardudi@gmail.com>
2024-12-01 01:35:07 +0530

    Update name

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -3,7 +3,7 @@ import ProfilePic from "@/images/dummy.png";
 
 
 export const BIO = {
-  name: "Aashruti Maurya",
+  name: "Aashruti",
   email: "Lorem@ipsum.com",
   intro:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu",
